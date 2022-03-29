a1681d7313d95bbb85b83463cf2d1f09262fd605
clement_jum <cjumeline@tutanota.com>
2022-03-30 00:29:05 +0200

    Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {FaceSnapListComponent} from "./Help/face-snap-list/face-snap-list.component";
 import {LandingPageComponent} from "./Help/landing-page/landing-page.component";
 import {SingleFaceSnapComponentComponent} from "./Help/single-face-snap-component/single-face-snap-component.component";
+import {AccueilComponent} from "./accueil/accueil.component";
 
 const routes: Routes = [
+  { path: 'Accueil', component: AccueilComponent },
   { path: 'facesnaps/:id', component: SingleFaceSnapComponentComponent },
   { path: 'facesnaps', component: FaceSnapListComponent },
   { path: '', component: LandingPageComponent }
